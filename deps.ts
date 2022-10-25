6eb0ae85469dd8ebddf66c1a4711c689ddfbc4f3
Octo8080 <okutannderuta0529@gmail.com>
2022-10-25 22:14:03 +0900

    0003

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { MiddlewareHandlerContext } from "https://deno.land/x/fresh@1.1.1/server.ts";
+export { type MiddlewareHandlerContext } from "https://deno.land/x/fresh@1.1.1/server.ts";
 export { nanoid } from "https://cdn.jsdelivr.net/npm/nanoid/nanoid.js";

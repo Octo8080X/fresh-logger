55de704dd4c9d49a2da554c7994b2723161b9ff2
Octo8080 <okutannderuta0529@gmail.com>
2023-08-24 00:56:06 +0900

    adaptation to Fresh 1.4 Plugin

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { type MiddlewareHandlerContext, type MiddlewareHandler } from "https://deno.land/x/fresh@1.3.1/server.ts";
+export { type MiddlewareHandlerContext, type MiddlewareHandler } from "https://deno.land/x/fresh@1.4.2/server.ts";
 export { type Plugin } from "https://deno.land/x/fresh@1.4.2/src/server/types.ts"
 export { nanoid } from "https://cdn.jsdelivr.net/npm/nanoid/nanoid.js";
